6e2063886d24823b9323e677c37ae7704f972433
hoangchitrung <trunghoang1124@gmail.com>
2026-02-25 09:55:01 +0700

    [remove] remove data

--- frontend/src/data/data.jsx
+++
@@ -1,7 +0,0 @@
-export const data = [
-    { id: 1, name: "Test 1", price: 1000, stock: 50 },
-    { id: 3, name: "Test 3", price: 1000, stock: 30 },
-    { id: 4, name: "Test 4", price: 1000, stock: 10 },
-    { id: 2, name: "Test 2", price: 1000, stock: 35 },
-    { id: 5, name: "Test 5", price: 1000, stock: 39 }
-];
