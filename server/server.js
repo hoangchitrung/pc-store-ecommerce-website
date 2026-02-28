3d65d1a345f640c575515294ce1b3638a44fd3cb
hoangchitrung <trunghoang1124@gmail.com>
2026-02-28 11:22:42 +0700

    [update] npm run dev for server-side messed up with .env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connection = require("./db");
+const connection = require("./config/db");
 
 const app = express();
 const PORT = 3000;
